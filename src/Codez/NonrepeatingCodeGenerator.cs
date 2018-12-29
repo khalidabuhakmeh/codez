0a06ce14bfc1034a86793e2ece7692238e06db85
Tim VanFosson <tvanfosson@gmail.com>
2018-12-29 10:00:38 -0600

    Stub nonrepeating code generator class and tests.

---
+++ src/Codez/NonrepeatingCodeGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Codez
+{
+    public class NonrepeatingCodeGenerator : ICodeGenerator
+    {
+        public ValueTask<string> GenerateAsync(int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<CodeGeneratorResult> TryGenerateAsync(int length)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
