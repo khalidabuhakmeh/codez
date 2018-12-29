098f86514961cffb555476407f4eb353b15a5e8a
Tim VanFosson <tvanfosson@gmail.com>
2018-12-29 11:40:39 -0600

    Add initial shuffle-based implementation to pass existing tests.

--- src/Codez/NonrepeatingCodeGenerator.cs
+++ src/Codez/NonrepeatingCodeGenerator.cs
@@ -1,13 +1,32 @@
-﻿using System;
+﻿using System.Linq;
 using System.Threading.Tasks;
+
+using Codez.Alphabets;
+using Codez.Randomizers;
+using Codez.StopWords;
+using Codez.Transformers;
+using Codez.Uniques;
 
 namespace Codez
 {
     public class NonrepeatingCodeGenerator : CodeGeneratorBase, ICodeGenerator
     {
+        public NonrepeatingCodeGenerator(CodeGeneratorOptions options = null,
+                             IAlphabet alphabet = null,
+                             IRandomizer randomizer = null,
+                             IUniqueness uniqueness = null,
+                             IStopWords stopWords = null,
+                             ITransformer transformer = null
+        )
+            : base(options, alphabet, randomizer, uniqueness, stopWords, transformer)
+        {
+        }
+
         protected override async Task<string> GenerateAttemptAsync(int length)
         {
-            throw new NotImplementedException();
+            var shuffledAlphabet = await alphabet.Characters.ShuffleAsync(randomizer);
+
+            return new string(shuffledAlphabet.Take(length).ToArray());
         }
     }
 }
