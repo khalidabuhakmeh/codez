3278462f9a6e63746863b6a940612dc3c9fd3458
Tim VanFosson <tvanfosson@gmail.com>
2018-12-29 09:39:19 -0600

    Renaming counting variable for attempts to avoid conflict with property holding count of characters in the alphabet

--- test/Tests/ListenerTests.cs
+++ test/Tests/ListenerTests.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Codez;
 using Codez.Alphabets;
@@ -14,11 +13,11 @@ namespace Tests
             var alphabet = new CountingAlphabet();
             var generator = new CodeGenerator(alphabet: alphabet);
             
-            Assert.Equal(0, alphabet.Count);
+            Assert.Equal(0, alphabet.Attempts);
 
             await generator.GenerateAsync(5);
             
-            Assert.Equal(1, alphabet.Count);
+            Assert.Equal(1, alphabet.Attempts);
         }
     }
 
@@ -29,14 +28,13 @@ namespace Tests
         {            
         }
         
-        public IReadOnlyList<char> Characters { get; }
         public Task OnBeforeAttempt(BeforeAttemptEvent @event)
         {
-            Count = @event.Attempt;
+            Attempts = @event.Attempt;
             return Task.FromResult(0);
         }
 
-        public int Count { get; private set; }
+        public int Attempts { get; private set; }
 
         public Task OnAfterAttempt(AfterAttemptEvent @event)
         {
